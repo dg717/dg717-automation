70a9ffc5bf553f9eee851b951250f55431eb875f
shidaschubert <shidaschubert@gmail.com>
2015-06-09 22:17:31 +0000

    fixed scope for meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,7 +7,7 @@ class Meeting < ActiveRecord::Base
   default_scope { order('start_time') }
 
   scope :for_room, -> { 
-            where("end_time >= '#{self.time_now}' and start_time <= '#{Time.now.end_of_day.in_time_zone(TIMEZONE)}'") 
+            where("end_time >= '#{self.time_now}' and start_time <= '#{Time.now.in_time_zone(TIMEZONE).end_of_day}'") 
           }
   scope :is_ongoing, ->{  
             where("start_time < '#{self.time_now}' and end_time > '#{self.time_now}'")
